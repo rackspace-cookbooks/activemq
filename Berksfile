b43824c5b9e1cca3d55273eacc426428dfdec3ae
Fernando Honig <fernando.honig@rackspace.co.uk>
2014-03-11 11:34:17 +0000

    Adapting for kitchen tests

--- Berksfile
+++ Berksfile
@@ -1,7 +1,12 @@
-site :opscode
 metadata
 
+cookbook 'ark',  git: 'git://github.com/opscode-cookbooks/ark.git'
+cookbook 'java', github: 'rackspace-cookbooks/java'
+
+cookbook 'monit', git: 'git://github.com/apsoto/monit.git'
+
 group :integration do
-  cookbook 'apt', '~> 2.0'
-  cookbook 'yum', '~> 2.0'
+  cookbook 'minitest-handler', git: 'git://github.com/btm/minitest-handler-cookbook.git'
+  cookbook 'rackspace_apt', github: 'rackspace-cookbooks/rackspace_apt'
+  cookbook 'rackspace_yum', github: 'rackspace-cookbooks/rackspace_yum'
 end
--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,15 @@
-name             'activemq'
-maintainer       'Opscode, Inc.'
-maintainer_email 'cookbooks@opscode.com'
+name             'rackspace_activemq'
+maintainer       'Rackspace'
+maintainer_email 'rackspace-cookbooks@rackspace.com'
 license          'Apache 2.0'
-description      'Installs activemq and sets it up as service'
+description      'Installs and configures activemq'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.3.4'
+version          '1.0.0'
 
-%w(ubuntu debian redhat centos suse).each do |os|
+%w(ubuntu debian redhat centos).each do |os|
   supports os
 end
 
 depends 'java', '~> 1.13'
+
+provides 'rackspace_activemq'
